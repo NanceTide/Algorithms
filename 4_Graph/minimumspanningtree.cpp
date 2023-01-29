fd97f93a3692279c3d3d53b7600a5f2d50f82641
NanceTide <NanceTide@outlook.com>
2023-01-29 19:42:58 +0800

    23.01.29..0

--- 4_Graph/minimumspanningtree.cpp
+++ 4_Graph/minimumspanningtree.cpp
@@ -1,8 +1,9 @@
 #include "graph_lib.cpp"
 #include <deque>
+#include <cfloat>
 using namespace std;
 
-template <typename T> class Heap {
+template <typename T> class Heap { // 优先队列
     public:
     vector<T> vec;
     int n;
@@ -43,18 +44,86 @@ template <typename T> class Heap {
     }
 };
 
+template <typename T> class IndexHeap { // 索引优先队列
+    public:
+    vector<T> keys;
+    vector<int> pq;
+    vector<int> qp;
+    int n;
+    IndexHeap(int capacity) : keys(capacity + 1), pq(capacity + 1, -1), qp(capacity + 1, -1), n(0) {}
+    IndexHeap() {}
+    void exch(int i, int j) { // public
+        swap(pq[i], pq[j]);
+        qp[pq[i]] = i;
+        qp[pq[j]] = j;
+    }
+    bool contains(int k) {
+        return qp[k] != -1;
+    }
+    void insert(int k, T t) {
+        if(contains(k))
+            return;
+        n++;
+        keys[k] = t;
+        pq[n] = k;
+        qp[k] = n;
+        swim(n);
+    }
+    int delMin() {
+        int minIndex = pq[1];
+        exch(1, n);
+        qp[pq[n]] = -1;
+        pq[n] = -1;
+        n--;
+        sink(1);
+        return minIndex;
+    }
+    T operator[](int k) {
+        if(!contains(k))
+            throw;
+        return keys[k];
+    }
+    T& min() {
+        return keys[pq[1]];
+    }
+    void del(int k) {
+        int index = qp[k];
+        exch(k, n);
+        qp[pq[n]] = -1;
+        pq[n] = -1;
+        n--;
+        sink(k);
+        swim(k);
+    }
+    void change(int k, T t) {
+        keys[k] = t;
+        swim(qp[k]);
+        sink(qp[k]);
+    }
+    void sink(int k) {
+        for(int j = k << 1; j <= n; k = j, j <<= 1) {
+            if(j + 1 <= n && keys[pq[j]] > keys[pq[j + 1]])
+                j++;
+            if(keys[pq[k]] < keys[pq[j]])
+                return;
+            exch(k, j);
+        }
+    }
+    void swim(int k) {
+        for(; k > 1 && keys[pq[k >> 1]] > keys[pq[k]]; k >>= 1)
+            exch(k >> 1, k);
+    }
+};
+
 class LazyPrimMST {
     public:
     vector<int> marked;
     deque<Edge> mst;
     Heap<Edge> heap;
-
     deque<Edge> operator()(EdgeGraph g)  {
         marked = vector<int>(g.V, 0);
         mst.clear();
-        heap.vec.clear();
-        heap.n = 0;
-        // heap.vec.push_back(Edge(0, 0, 0));
+        heap.clear(Edge());
         visit(g, 0);
         while(heap.n) {
             Edge tmp = heap.delMin();
@@ -76,10 +145,46 @@ class LazyPrimMST {
             if(!marked[c.other(i)])
                 heap.insert(c);
     }
-}LPrim;
+}lazyPrim;
+
+class PrimMST {
+    public:
+    vector<Edge> edgeTo;
+    vector<double> distTo;
+    vector<int> marked;
+    IndexHeap<double> pq;
+    vector<Edge> operator()(EdgeGraph g) {
+        edgeTo = vector<Edge>(g.V, Edge(0, 0, 0.0));
+        distTo = vector<double>(g.V, DBL_MAX);
+        marked = vector<int>(g.V, 0);
+        pq = IndexHeap<double>(g.V);
+        distTo[0] = 0.0;
+        pq.insert(0, 0.0);
+        while(pq.n)
+            visit(g, pq.delMin());
+        return edgeTo;
+    }
+    void visit(EdgeGraph g, int i) {
+        marked[i] = 1;
+        for(Edge e : g.adj[i]) {
+            int w = e.other(i);
+            if(marked[w])
+                continue;
+            if(e.weight < distTo[w]) {
+                edgeTo[w] = e;
+                distTo[w] = e.weight;
+                if(pq.contains(w))
+                    pq.change(w, e.weight);
+                else
+                    pq.insert(w, e.weight);
+            }
+        }
+    }
+}prim;
 
 int main() {
 
+    ios::sync_with_stdio(false);
     int V, E;
     cin >> V >> E;
     int v, w;
@@ -92,7 +197,7 @@ int main() {
 
     cout << '\n';
 
-    for(auto e : LPrim(g))
+    for(auto e : prim(g))
         cout << e.v << ' ' << e.w << ' ' << e.weight << '\n';
-    
+
 }
